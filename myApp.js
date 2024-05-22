d7d33e003e8d646dd8a5bffbf96c70179c685767
James Nascimento <james.nascimento94@yahoo.com>
2024-05-22 02:28:02 +0000

    finished challenges

--- myApp.js
+++ myApp.js
@@ -14,6 +14,7 @@ app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}}));
 
 
 
